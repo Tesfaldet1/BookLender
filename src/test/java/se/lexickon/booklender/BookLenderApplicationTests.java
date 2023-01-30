03937a29e47fb6b0fb332a4926920838c23acd8e
Tesfaldet Weldemicheal <tweldemicheal@gmail.com>
2023-01-30 12:14:54 +0100

    Initial commit

---
+++ src/test/java/se/lexickon/booklender/BookLenderApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexickon.booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
